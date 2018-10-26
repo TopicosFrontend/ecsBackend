58e8fb764c9814f1141729e2cb2d64cf47030521
Alejandro Salgado G <asalgad2@eafit.edu.co>
2018-10-26 11:17:36 -0500

    Adding support registration

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from support.models import SupportInfo
 
-# Register your models here.
+admin.site.register(SupportInfo)
--- support/views.py
+++ support/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
+from django.contrib.auth.models import User
+from support.models import SupportInfo
+
 # salgado
 def index(request): 
     return HttpResponse("Hello from support backend")
@@ -33,8 +36,24 @@ def logout(request):
     return JsonResponse({"state": "true", "msg": "logout successful"})
 
 # salgado
+@csrf_exempt
 def register(request):
-    return HttpResponse("support register")
+    try:
+        username = request.user.get_username()
+        user_support = SupportInfo.objects.get(username=username)
+    except SupportInfo.DoesNotExist:
+        return JsonResponse({"state": "false", "msg": "user is not a support member"})
+
+    data = request.POST
+    username = data["user"]
+    password = data["password"]
+
+    user = User.objects.create_user(username=username, password=password)
+    user.save()
+    support = SupportInfo(username=user.get_username())
+    support.save()
+
+    return JsonResponse({"state": "false", "msg": "user created"})
 
 # salgado
 def show_collector(request):
