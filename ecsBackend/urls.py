e0977af3f738bb2ee526f820bd13fe7632bfa5f8
Alejandro Salgado G <asalgad2@eafit.edu.co>
2018-11-15 17:49:25 -0500

    Adding configuration to send static files to production

--- ecsBackend/urls.py
+++ ecsBackend/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('support/', include('support.urls')),
     path('collector/', include('collector.urls')),
     path('user/', include('user.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
